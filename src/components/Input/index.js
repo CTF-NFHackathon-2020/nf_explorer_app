f046951bf74804334db68abad2becea820fd8adb
Matheus Mello <matheusmello_22@hotmail.com>
2020-11-07 18:23:57 -0300

    Create Medication page

---
+++ src/components/Input/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import * as S from "./styles";
+
+export default function Input({ navigation }) {
+  return <S.Input />;
+}
--- src/pages/Medication/index.js
+++ src/pages/Medication/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { Dimensions, SafeAreaView } from "react-native";
 import * as S from "./styles";
-import { colors, typography } from "./../../settings/default";
-import { artists } from "../../mock";
-import { LinearGradient } from "expo-linear-gradient";
-import { useEffect } from "react";
 import { FontAwesome, Entypo } from "@expo/vector-icons";
+import { Input } from "../../components";
 
 export default function Main({ navigation }) {
   function renderSpecificPercentage(index, total) {
@@ -49,42 +46,29 @@ export default function Main({ navigation }) {
             backgroundColor: "rgb(227, 227, 227)",
             flexGrow: 1,
             paddingHorizontal: 20,
-            paddingVertical: 40,
             marginHorizontal: 20,
             alignSelf: "center",
             width: Dimensions.get("window").width - 40,
             borderRadius: 20,
           }}
         >
-          {[1, 2, 3].map((el, index) => (
+          {[
+            "Medication",
+            "Date Prescribed",
+            "Dosage",
+            "Frequency",
+            "Notes",
+          ].map((el, index) => (
             <S.MinimumContainer
               style={{
                 backgroundColor: "rgb(227, 227, 227)",
                 flexGrow: 1,
                 paddingHorizontal: 20,
-                borderLeftWidth: 3,
-                borderLeftColor: "rgb(225, 145, 124)",
-                paddingBottom: index == 2 ? 0 : 50,
+                paddingVertical: 20,
               }}
             >
-              <FontAwesome
-                name="circle"
-                style={{
-                  position: "absolute",
-                  left: -9,
-                  top: renderSpecificPercentage(index, 2),
-                }}
-                color={"rgb(225, 145, 124)"}
-                size={20}
-              />
-
-              <S.SubTitle style={{ fontWeight: "500" }}>
-                Oct 20, 2020
-              </S.SubTitle>
-              <S.SubTitle style={{ fontWeight: "bold" }}>
-                NF1 Annual Checkup
-              </S.SubTitle>
-              <S.SubTitle>New York, NY</S.SubTitle>
+              <S.SubTitle style={{ fontWeight: "500" }}>{el}</S.SubTitle>
+              <Input />
             </S.MinimumContainer>
           ))}
         </S.MinimumContainer>
